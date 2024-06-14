647d934ea15dbb25f0a6508fb8b726d173c97e6e
ChiLo <pyha1994@yandex.ru>
2024-06-14 22:09:10 +0300

    - сделал страниццу создания персонажа

---
+++ Game/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Game</title>
+</head>
+<body>
+<form action="#" method="get">
+<div>
+    <p>Создание песнонажа:</p>
+</div>
+<div>
+    <p>Придумайте ник</p>
+    <input type="text" placeholder="Введите ник">
+</div>
+<div>
+    <p>Выберите класс:</p>
+
+    <div>
+        <input type="radio" name="class" value="Archer"/> Archer
+    </div>
+    <div>
+        <input type="radio" name="class" value="Mage"/> Mage
+    </div>
+    <div>
+        <input type="radio" name="class" value="Warrior"/> Warrior
+    </div>
+</div>
+<div>
+    <button>В игру!</button>
+</div>
+</form>
+<?php
+
+
+?>
+</body>
+</html>
