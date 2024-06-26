411bd69dd41b9adc912104e75c112b3a2c8950e9
ChiLo <pyha1994@yandex.ru>
2024-06-26 20:33:14 +0300

    - почистил трэш

--- php_2/исходники/getConvertKelToCel.php
+++
@@ -1,6 +0,0 @@
-<?php
-function getConvertKelToCel($kel)
-{
-    $cel = $kel - 273.15;
-    return $cel;
-}
--- php_2/исходники/getConvertKmChToMS.php
+++
@@ -1,6 +0,0 @@
-<?php
-function getConvertKmChToMS($km_ch)
-{
-    $m_s = $km_ch / 3.6;
-    return $m_s;
-}
